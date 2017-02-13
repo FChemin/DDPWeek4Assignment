17401be92040cc02a1d139fedac846a8e72da172
FChemin <chemin.florian@gmail.com>
2017-02-13 14:01:55 +0100

    added about page on UI

---
+++ about.md
@@ -0,0 +1,9 @@
+Welcome !
+
+This site is providing a tool to compute the price of the rent.
+
+We required you to provide your apartment details: first, the area; second, the number of rooms.
+
+The result contains a table with a price fitted to our linear model and an upper and lower bound from a 95% confidence interval.
+
+Source code for ui.R and server.R files are available on the GitHub repo: https://github.com/FChemin/DDPWeek4Assignment
--- ui.R
+++ ui.R
@@ -10,11 +10,16 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(titlePanel("Rent Calculator"), br(), "Find", em("how much"), 
-                  "you could rent a place", "in", strong("Brest, France"),"!", br(), br(),
-                  sidebarLayout(sidebarPanel(img(src = "http://florianchemin.com/wp-content/uploads/2017/02/buildings-498198_640.jpg"), 
+shinyUI(navbarPage("Best price to rent your place", 
+                   tabPanel("Home", titlePanel("Rent Calculator"), br(), "Find", 
+                            em("how much"), "you could rent a place", "in", 
+                            strong("Brest, France"),"!", br(), br(), 
+                            sidebarLayout(sidebarPanel(img(src = "http://florianchemin.com/wp-content/uploads/2017/02/buildings-498198_640.jpg"), 
                                              sliderInput("areaInput", "Area of the place to rent", min = 0, 
                                                          max = 200, value = 100, post = "m²"), 
                                              numericInput("roomsInput", "Number of rooms at the place", 
                                                           value = 4, min = 1, max = 8, step = 1)), 
-                                mainPanel(tableOutput("Inputvals"),tableOutput("Results")))))
+                                mainPanel(tableOutput("Inputvals"),tableOutput("Results")))), 
+                   tabPanel("About",
+                            mainPanel(
+                                includeMarkdown("about.md")))))
